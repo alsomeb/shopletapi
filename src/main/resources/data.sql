e81a44dcbd68d4e864b1bada3f45e910a8e6f188
alsomeb <alsomeb@gmail.com>
2023-05-09 13:29:49 +0200

    - added more seed in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,5 @@ insert into shopping_lists (id, description, added)
 values
     (10001, 'Groceries for dinner with dad', '2023-08-23'),
     (10002, 'Buy snacks for the weekend', '2023-05-23'),
-    (10003, 'Birthday order', current_date());
+    (10003, 'Birthday order', current_date()),
+    (10004, 'Test order', current_date());
